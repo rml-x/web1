50a016d917919fdd0eacb35439d2b337e370c85b
rml-x <cristiane.xavier@aluno.riogrande.ifrs.edu.br>
2026-09-02 01:59:40 -0300

    add:index.php

--- php/conexao.php
+++ php/conexao.php
@@ -2,7 +2,7 @@
 
 
 <?php
-    $conn = mysqli_connect("localhost", "root", "mysqluser", "usuarios");
+    $conn = mysqli_connect("localhost", "root", "", "banco_de_disciplinas");
 
     if (!$conn) {
         die("Falha na conexão: " . mysqli_connect_error());
